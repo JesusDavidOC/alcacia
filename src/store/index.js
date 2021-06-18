8acd265fb1553714ba5ac8d45502289d21f2210e
JesusDavidOC <jesus.oteroc@autonoma.edu.co>
2021-06-17 23:20:15 -0500

    primera version alcancia

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    monedas: []
   },
-  mutations: {
+  mutations: {    
   },
   actions: {
+    addMoneda({ state }, payload) {      
+      state.monedas.push(payload);
+      localStorage.setItem('monedas', JSON.stringify(state.monedas))
+    },
+    setMonedas({state}, payload){      
+      state.monedas = payload
+    },
+    eliminarMoneda({state}, payload){
+      for (const key in state.monedas) {
+          if(state.monedas[key].valor == payload){
+            state.monedas.splice(key, 1);
+            break;
+          }
+      }
+      localStorage.setItem('monedas', JSON.stringify(state.monedas))
+      
+    }
   },
   modules: {
   }
